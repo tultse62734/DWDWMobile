5ae7880cb9c4503da5f78b81b95d19edb5e0de51
doraemon4324 <47148721+doraemon4324@users.noreply.github.com>
2020-06-19 14:33:12 +0700

    View and Search assigned location of account  manager, worker.
    Hiển thị và search location theo ID được chỉ định tùy theo account đang đăng nhập theo role manager, worker

--- DWDW-WebAPI/DWDW-WebAPI/Controllers/LocationsController.cs
+++ DWDW-WebAPI/DWDW-WebAPI/Controllers/LocationsController.cs
@@ -6,6 +6,7 @@ using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
 using System.Web.Http;
 using System.Web.Http.Description;
 using DWDW_WebAPI.Models;
@@ -36,7 +37,32 @@ namespace DWDW_WebAPI.Controllers
             return Ok(searchedLocation);
         }
 
-        
+        //Get assigned Location for manager and worker
+        [Authorize(Roles = "2, 3")]
+        [HttpGet]
+        [Route("api/subaccount/locationList")]
+        public IHttpActionResult GetAssignedLocations()
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var ID = identity.Claims.FirstOrDefault(c => c.Type == "ID").Value;
+            int accountID = Convert.ToInt32(ID);
+            var locationList = db.Locations.Where(a => a.UserLocations.Any(b => b.userId == accountID)).ToList();
+            return Ok(locationList);
+        }
+
+        //Search Assigned Location for manager and worker
+        [Authorize(Roles = "2,3")]
+        [HttpGet]
+        [Route("api/subaccount/locationFinder/{locationID}")]
+        public IHttpActionResult FindAssignedLocations(int locationID)
+        {
+            var identity = (ClaimsIdentity)User.Identity;
+            var ID = identity.Claims.FirstOrDefault(c => c.Type == "ID").Value;
+            int accountID = Convert.ToInt32(ID);
+            var locationList = db.Locations.Where(a => a.UserLocations.Any(b => b.userId == accountID)).ToList();
+            var searchLocation = locationList.FirstOrDefault(x => x.locationId == locationID);
+            return Ok(searchLocation);
+        }
 
         // PUT: api/Locations/5
         [ResponseType(typeof(void))]
