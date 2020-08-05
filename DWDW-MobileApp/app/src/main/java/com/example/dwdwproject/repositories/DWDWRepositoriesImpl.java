a1b3ae47f6b98d0d0105bef053725a4c163acf40
doraemon4324 <47148721+doraemon4324@users.noreply.github.com>
2020-08-05 16:16:28 +0700

    Revert "[DWDW-mobile] Error Message"
    This reverts commit b93fb73b4f18abcacd707c8049de7083ecdc8e23.

--- DWDW-MobileApp/app/src/main/java/com/example/dwdwproject/repositories/DWDWRepositoriesImpl.java
+++ DWDW-MobileApp/app/src/main/java/com/example/dwdwproject/repositories/DWDWRepositoriesImpl.java
@@ -5,8 +5,6 @@ import com.example.dwdwproject.ResponseDTOs.LoginDTO;
 import com.example.dwdwproject.ResponseDTOs.UserDTO;
 import com.example.dwdwproject.models.Device;
 import com.example.dwdwproject.models.ReponseDTO;
-import com.example.dwdwproject.models.ResultReponse;
-import com.example.dwdwproject.models.ResultReponse1;
 import com.example.dwdwproject.models.User;
 import com.example.dwdwproject.utils.CallBackData;
 import com.example.dwdwproject.utils.ClientApi;
@@ -49,23 +47,22 @@ public class DWDWRepositoriesImpl implements DWDWRepositories {
             @Override
             public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
                 KProgressHUDManager.dismiss(mContext, khub);
+                if (response.code() == 200 && response.body() != null) {
                     try {
                         String result = response.body().string();
-                        Type type = new TypeToken<ResultReponse>() {
+                        Type type = new TypeToken<ReponseDTO>() {
+
                         }.getType();
                         //call response to get value data
-                        ResultReponse resultReponse = new Gson().fromJson(result, type);
-                        if (resultReponse.getStatusCode() == 200) {
-                            ReponseDTO mReponseDTO = new ReponseDTO();
-                            mReponseDTO.setToken(resultReponse.getData());
-                            callBackData.onSucess(mReponseDTO);
-                        }
-                        else {
-                            callBackData.onFail(resultReponse.getErrorMessage());
-                        }
+                        ReponseDTO reponseDTO = new Gson().fromJson(result, type);
+                        callBackData.onSucess(reponseDTO);
+
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                } else {
+                    callBackData.onFail(response.message());
+                }
             }
             @Override
             public void onFailure(Call<ResponseBody> call, Throwable t) {
@@ -131,7 +128,7 @@ public class DWDWRepositoriesImpl implements DWDWRepositories {
                 if (response.code() == 200 && response.body() != null) {
                     try {
                         String result = response.body().string();
-                        Type type = new TypeToken<ResultReponse1<UserDTO>>() {
+                        Type type = new TypeToken<UserDTO>() {
 
                         }.getType();
                         //call response to get value data
